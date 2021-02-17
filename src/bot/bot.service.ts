97c57eb6954b1b0ae29969fe5368da80fd0ab11e
Sergey Karpenko <sergey.karpenko.dev@gmail.com>
2021-02-17 12:13:04 +0200

    update message action

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -2,53 +2,14 @@ import * as youtube from '../youtube/youtube.service';
 import * as host from '../youtube';
 import { UnknownHostError } from './errors';
 import { YoutubeInfo } from '../youtube';
-import { InlineKeyboardMarkup } from 'telegraf/typings/telegram-types';
-import { logger } from '../config';
 
 const SIZE_MB = 5e7;
-
-export type YoutubeInfoWithKeybord = {
-  message: string;
-  keyboard: InlineKeyboardMarkup;
-};
-
-const getYoutubeInfo = async (url: URL): Promise<YoutubeInfoWithKeybord> => {
-  if (!host.checkHost(url)) {
-    throw new UnknownHostError(`Unknown host ${url.host}`);
-  }
-
-  const info: YoutubeInfo = await youtube.getInfo(url);
-
-  if (info.filesize > SIZE_MB) {
-    throw new Error('Long file size');
-  }
-
-  const artist = info.artist ? info.artist.split(',').shift() : 'Unknown';
-  const message = `${artist} - ${info.title}`;
-  const buttons = [
-    {
-      text: 'Save',
-      callback_data: url.toString(),
-    },
-  ];
-  const keyboard: InlineKeyboardMarkup = {
-    inline_keyboard: [[...buttons]],
-  };
-  return {
-    message,
-    keyboard,
-  };
-};
 
 const getYoutubeAudio = async (url: URL) => {
   if (!host.checkHost(url)) {
     throw new UnknownHostError(`Unknown host ${url.host}`);
   }
   const info: YoutubeInfo = await youtube.getInfo(url);
-
-  if (info.filesize > SIZE_MB) {
-    throw new Error('Long file size');
-  }
 
   const artist = info.artist ? info.artist.split(',').shift() : 'Unknown';
   return {
@@ -61,4 +22,4 @@ const getYoutubeAudio = async (url: URL) => {
   };
 };
 
-export { getYoutubeAudio, getYoutubeInfo };
+export { getYoutubeAudio };
--- src/bot/message.action.ts
+++ src/bot/message.action.ts
@@ -3,36 +3,17 @@ import { Context } from 'telegraf';
 import * as service from './bot.service';
 import { bot } from './bot';
 
-type UpdateContext = Context & {
-  update: {
-    callback_query: any;
-  };
-};
-
 bot.on('message', async (ctx: Context) => {
   const { message }: any = ctx.update;
   const { text } = message;
 
   try {
     const url = new URL(text);
-    const info = await service.getYoutubeInfo(url);
-    await ctx.reply(info.message, {
-      reply_markup: info.keyboard,
-    });
-  } catch (error) {
-    logger.warn(error.message);
-    await ctx.reply(error.message);
-  }
-});
-
-bot.on('callback_query', async (ctx: UpdateContext) => {
-  try {
-    const { data, message } = ctx.update.callback_query;
-    const url = new URL(data);
     const audio = await service.getYoutubeAudio(url);
     ctx.deleteMessage(message.message_id);
     ctx.replyWithAudio(audio.stream, audio.extra);
   } catch (error) {
     logger.warn(error.message);
+    await ctx.reply(error.message);
   }
 });
